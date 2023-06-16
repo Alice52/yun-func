da0a216f5a8bd9472de1d9c79ad67dd5d242b996
alice52 <zzhang_xz@163.com>
2023-06-16 17:39:00 +0800

    feat(backend): add aliyun cf sample

---
+++ backend/yun-fc-starter/fc-start-springboot/readme.md
@@ -0,0 +1,6 @@
+## feature
+
+1. http web api
+2. import swagger
+3. import spring and mybatis
+4. import kafka
---
+++ backend/yun-fc-starter/fc-start-springboot/src/test/java/top/hubby/fc/web/WebFrameworkApplicationTests.java
@@ -0,0 +1,20 @@
+package top.hubby.fc.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+import top.hubby.fc.web.controller.HelloController;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFrameworkApplicationTests {
+
+    @Test
+    public void testWelcome() {
+        assertEquals(HttpStatus.OK, new HelloController().welcome().getStatusCode());
+    }
+}
