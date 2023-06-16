5aad40a83b3c4a8a0b6225b36c3f5039f9d3a909
alice52 <zzhang_xz@163.com>
2023-06-16 23:53:13 +0800

    feat(backend): faas integrate with mysql

--- backend/yun-fc-starter/fc-start-springboot/src/main/java/top/hubby/fc/web/WebFrameworkApplication.java
+++ backend/yun-fc-starter/fc-start-springboot/src/main/java/top/hubby/fc/web/WebFrameworkApplication.java
@@ -2,7 +2,9 @@ package top.hubby.fc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 public class WebFrameworkApplication {
 
